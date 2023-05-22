d094596ee4f97993b1d82d9bfb1818d2fb05bb21
Abdul Qadir <abdulqadirshaikh101@gmail.com>
2023-05-22 20:19:19 +0500

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Shade Pro - Premium Landing Page UI Kit</title>
+    <title>
+        Remotely Hire
+    </title>
     <link rel="shortcut icon" href="image/png/favicon.png" type="image/x-icon">
     <!-- Bootstrap , fonts & icons  -->
     <link rel="stylesheet" href="/theme/ShadePro/html/light/css/bootstrap.css">
